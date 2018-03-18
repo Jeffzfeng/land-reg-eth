ecfa3b1120bce417bc2f1606b555d87cd847b892
solandev <prasoon.jha@mail.utoronto.ca>
2018-03-18 08:28:57 -0700

    clean-up; DJ P^2

--- Solander/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Users = artifacts.require("Users");
-var LandTransfers = artifacts.require("LandTransfers")
-
-module.exports = function(deployer) {
-	deployer.deploy(Users);
-	deployer.deploy(LandTransfers)
-};
