963c7aa6b2bcc223e5f1990f46cf1b683df16eb4
jeffzfeng <jefff.feng@mail.utoronto.ca>
2018-03-20 18:48:23 -0400

    changed some styling in header

--- solander_react/src/components/Header.js
+++ solander_react/src/components/Header.js
@@ -19,7 +19,7 @@ export default class Header extends Component {
                     <li><Link to='/'><i className="fa fa-institution"></i><br/>Home</Link></li>
                     <li><Link to='/profile'><i className="fa fa-user-circle"></i><br/>Profile</Link></li>
                     <li><Link to='/register'><i className="fa fa-clipboard"></i><br/>Register</Link></li>
-                    <li><Link to='/transfer'><i className="fa fa-share"></i><br/>Land Transfer</Link></li>
+                    <li><Link to='/transfer'><i className="fa fa-share"></i><br/>Transfer</Link></li>
                 </ul>
             </nav>
         </div>
